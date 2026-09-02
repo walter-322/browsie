eb2b233a449b033f6e9cb8765c594a824f61cda4
Walter <walter.muller.1708@gmail.com>
2026-09-02 11:02:19 +0200

    Removed Vendor in Configure.sh

--- browser/branding/unofficial/configure.sh
+++ browser/branding/unofficial/configure.sh
@@ -3,6 +3,5 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_DISPLAYNAME=Browsie
-MOZ_APP_VENDOR="Corex Team"
 MOZ_MACBUNDLE_ID=org.corexteam.browsie
 MOZ_APP_REMOTINGNAME=Browsie
